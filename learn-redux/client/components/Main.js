d321d22b8b9976c88c1077a7ce27330704c63e92
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-19 18:24:22 +0100

    #11: Display Redux state inside components (posts in PhotoGrid)

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default function Main({ children }) {
+export default function Main(props) {
   return (
     <div>
       <h1>
         <Link to="/">Reduxstagram</Link>
       </h1>
 
-      {children}
+      {React.cloneElement({ ...props }.children, { ...props })}
     </div>
   );
 }
--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Photo from './Photo';
 
 export default function PhotoGrid(props) {
-  return <div className="photo-grid">PhotoGrid</div>;
+  return (
+    <div className="photo-grid">
+      {props.posts.map((post, i) => (
+        <Photo key={i} {...props} i={i} post={post} />
+      ))}
+    </div>
+  );
 }
