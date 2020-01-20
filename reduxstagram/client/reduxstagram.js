7622ccfca19d1ec3f79eb675a2b5e56b52716f92
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-20 14:18:24 +0100

    #17: Configure error tracking w/ Sentry

--- reduxstagram/client/reduxstagram.js
+++ reduxstagram/client/reduxstagram.js
@@ -8,6 +8,16 @@ import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
 import './styles/style.styl';
 
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url, { tags: { custom_tag: 'value' } }).install();
+
+// logException(new Error('Error!'), { email: 'user@example.com' });
+
+// Raven.captureMessage('Some error');
+// Raven.showReportDialog();
+
 const router = (
   <Provider store={store}>
     <Router history={history}>
